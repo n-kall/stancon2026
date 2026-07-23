99a2055257ac3d68abbf755ee99b68cca5b158da
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-07-23 15:59:43 +0300

    add orcid

--- scripts/mock_mcmc.r
+++ scripts/mock_mcmc.r
@@ -1,25 +1,24 @@
-
 library(ncdf4)
 library(posterior)
 
 nc <- nc_open("../models/prerun/sample.nc")
 
-good_sample  <- ncvar_get(nc, "good_sample")
+good_sample <- ncvar_get(nc, "good_sample")
 bad_sample_0 <- ncvar_get(nc, "bad_sample_0")
 bad_sample_1 <- ncvar_get(nc, "bad_sample_1")
 nc_close(nc)
 
-n_draw  <- nrow(good_sample)
+n_draw <- nrow(good_sample)
 n_chain <- ncol(good_sample)
 
 arr <- array(
-  data     = c(good_sample, bad_sample_0, bad_sample_1),
-  dim      = c(n_draw, n_chain, 3),
-  dimnames = list(
-    iteration = NULL,
-    chain     = NULL,
-    variable  = c("good_sample", "bad_sample_0", "bad_sample_1")
-  )
+    data = c(good_sample, bad_sample_0, bad_sample_1),
+    dim = c(n_draw, n_chain, 3),
+    dimnames = list(
+        iteration = NULL,
+        chain = NULL,
+        variable = c("good_sample", "bad_sample_0", "bad_sample_1")
+    )
 )
 
 sample <- as_draws_array(arr)
