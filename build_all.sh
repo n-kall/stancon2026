f4048a0dd37fb978f1ebeb56d981fac3d314b613
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-08-02 23:35:29 +0300

    cleanup build process

--- build_all.sh
+++ build_all.sh
@@ -3,31 +3,17 @@
 uv run quarto render book
 uv run quarto render slides
 
-uv run quarto render exercises \
-   --profile r \
-   --output exercises-r.ipynb
+uv run quarto render exercises/exercises.qmd \
+   --profile r
 
-uv run quarto convert exercises-r.ipynb \
-       --output exercises-r.qmd
+uv run quarto convert exercises/rendered/exercises-r.ipynb \
+    --output exercises/rendered/exercises-r.qmd \
 
 uv run quarto render exercises \
   --profile python \
-  --output exercises-python.ipynb
+
+uv run quarto render exercises/exercises.qmd \
+  --profile rsolutions
 
 uv run quarto render exercises \
-  --profile r-solutions \
-  --to html \
-  --execute \
-  -M execute.output:true \
-  -M execute.echo:true \
-  -M execute.warning:false \
-  --output solutions-r.html
-
-uv run quarto render exercises \
-  --profile python-solutions \
-  --to html \
-  --execute \
-  -M execute.output:true \
-  -M execute.echo:true \
-  -M execute.warning:false \
-  --output solutions-python.html
+  --profile pythonsolutions
