b34608a8b1f80df94b61eb204536ac8e23c56364
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-08-02 16:07:16 +0300

    add solutions to exercises

--- exercises/build_exercises.sh
+++ exercises/build_exercises.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 # Generate the R Jupyter notebook
 quarto render exercises.qmd \
   --profile r \
@@ -17,3 +20,22 @@ quarto render exercises.qmd \
 # Convert the Python notebook to a quarto document
 quarto convert exercises-python.ipynb \
   --output exercises-python.qmd
+
+# Render solutions
+quarto render exercises.qmd \
+  --profile r-solutions \
+  --to html \
+  --execute \
+  -M execute.output:true \
+  -M execute.echo:true \
+  -M execute.warning:false \
+  --output solutions-r.html
+
+quarto render exercises.qmd \
+  --profile python-solutions \
+  --to html \
+  --execute \
+  -M execute.output:true \
+  -M execute.echo:true \
+  -M execute.warning:false \
+  --output solutions-python.html
