2aad58d349cefb9120efd794082b1f145f2d6c93
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-08-02 22:38:54 +0300

    split into three different quarto projects

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-uv run quarto render
-uv run quarto render slides.qmd
-uv run bash build_exercises.sh 
+uv run quarto render book
+uv run quarto render slides
 
+uv run quarto render exercises \
+   --profile r \
+   --output exercises-r.ipynb
 
+uv run quarto convert exercises-r.ipynb \
+       --output exercises-r.qmd
+
+uv run quarto render exercises \
+  --profile python \
+  --output exercises-python.ipynb
+
+uv run quarto render exercises \
+  --profile r-solutions \
+  --to html \
+  --execute \
+  -M execute.output:true \
+  -M execute.echo:true \
+  -M execute.warning:false \
+  --output solutions-r.html
+
+uv run quarto render exercises \
+  --profile python-solutions \
+  --to html \
+  --execute \
+  -M execute.output:true \
+  -M execute.echo:true \
+  -M execute.warning:false \
+  --output solutions-python.html
